3fd20bf2c6cc77ad9a7dce1bf6b29d920c750731
NoeliaIntriago <noinsan@espol.edu.ec>
2025-05-25 16:53:34 -0500

    chore: actualizar dependencias de proyecto

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { fileURLToPath, URL } from 'node:url'
-
+import Components from 'unplugin-vue-components/vite'
+import { PrimeVueResolver } from 'unplugin-vue-components/resolvers'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
@@ -11,10 +12,13 @@ export default defineConfig({
     vue(),
     vueJsx(),
     vueDevTools(),
+    Components({
+      resolvers: [PrimeVueResolver()],
+    }),
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
 })
