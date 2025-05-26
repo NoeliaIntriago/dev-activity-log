2e2c8fac23338bc57d96798abf2d4d4e1765032e
NoeliaIntriago <noinsan@espol.edu.ec>
2025-05-25 20:53:35 -0500

    chore: implementar componente ActivityForm en componente GeneralActivitySection para manejo de actividades

--- src/components/report/GeneralActivitySection.vue
+++ src/components/report/GeneralActivitySection.vue
@@ -6,28 +6,7 @@
     </template>
 
     <template #content>
-      <div class="grid">
-        <div class="col-12 lg:col-4">
-          <InputText
-            id="descripcion"
-            v-model="activity.title"
-            type="text"
-            class="w-full"
-            placeholder="Título de la actividad"
-          />
-        </div>
-        <div class="col-12 lg:col-8">
-          <Textarea
-            v-model="activity.description"
-            rows="5"
-            class="w-full"
-            placeholder="Descripción de la actividad"
-          />
-        </div>
-      </div>
-      <div class="flex justify-content-end mb-2">
-        <Button label="Agregar" icon="pi pi-plus" class="p-button-outlined" @click="addActivity" />
-      </div>
+      <activity-form @add="($event) => $emit('add-activity', $event)" />
       <div>
         <DataTable :value="props.activities" resizable-columns>
           <template #empty>
@@ -42,7 +21,7 @@
               <Button
                 icon="pi pi-trash"
                 class="p-button-danger p-button-outlined"
-                @click="emit('remove-activity', slotProps.rowIndex)"
+                @click="$emit('remove-activity', slotProps.rowIndex)"
               />
             </template>
           </Column>
@@ -53,30 +32,9 @@
 </template>
 
 <script setup lang="ts">
-import type { Activity } from './types'
-import { ref } from 'vue'
-
-const activity = ref<Activity>({
-  title: '',
-  description: '',
-})
+import ActivityForm from '../ActivityForm.vue'
 
 const props = defineProps<{
   activities: Activity[]
 }>()
-const emit = defineEmits<{
-  (e: 'add-activity', activity: Activity): void
-  (e: 'remove-activity', index: number): void
-}>()
-
-const addActivity = () => {
-  if (activity.value.title && activity.value.description) {
-    // Emit the activity to the parent component
-    emit('add-activity', { ...activity.value })
-    // Reset the activity object
-    activity.value = { title: '', description: '' }
-  } else {
-    alert('Por favor completa todos los campos.')
-  }
-}
 </script>
