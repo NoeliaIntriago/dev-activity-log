ad59a5993c19c2a16b5e9ad481d66410ddbb21f1
NoeliaIntriago <noinsanc@espol.edu.ec>
2025-06-15 11:47:32 -0500

    fix: corregir `v-model` bindings en componente `MetadataInputSection` #1

--- src/components/report/MetadataInputSection.vue
+++ src/components/report/MetadataInputSection.vue
@@ -3,32 +3,27 @@
     <label for="ownerName" class="font-bold"> Nombre del Responsable: </label>
     <InputText
       id="ownerName"
-      v-model="ownerName"
+      :modelValue="owner"
       class="w-full"
       placeholder="Ingrese el nombre del responsable del reporte"
-      @blur="$emit('update:owner', ownerName.trim())"
+      @update:modelValue="$emit('update:owner', ($event || '').trim())"
     />
   </div>
   <div class="col-12 lg:col-6">
     <label for="ruc" class="font-bold"> RUC: </label>
     <InputText
       id="ruc"
-      v-model="ownerRuc"
+      :modelValue="ruc"
       class="w-full"
       placeholder="Ingrese el RUC del responsable del reporte"
-      @blur="$emit('update:ruc', ownerRuc.trim())"
+      @update:modelValue="$emit('update:ruc', ($event || '').trim())"
     />
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue'
-
 defineProps<{
   owner: string
   ruc: string
 }>()
-
-const ownerName = ref<string>('')
-const ownerRuc = ref<string>('')
 </script>
